9f4d378067407ebee8d025f810019ec31e21568f
kamaero <aerokamero@gmail.com>
2026-01-15 09:57:52 +0500

    Исправлен config.py (удален SQL код)

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
-# Author: Sergey Akulov
-# GitHub: https://github.com/serg-akulov
-
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные из .env файла
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -18,10 +14,3 @@ BOT_ADMIN_PASSWORD = os.getenv("BOT_ADMIN_PASSWORD")
 
 GOOGLE_SHEET_CREDENTIALS = "path/to/your/service_account.json"
 GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1XXXXX/edit"
-
-CREATE TABLE IF NOT EXISTS `bot_config` (
-  `key` varchar(100) NOT NULL,
-  `value` text NOT NULL,
-  `description` varchar(255) DEFAULT '',
-  PRIMARY KEY (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
